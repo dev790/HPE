234f6897db3e983f220508354a203b2b0449dd7f
Dev Nagda <73869195+dev790@users.noreply.github.com>
2022-03-30 21:15:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    document.querySelector('.target-burger').addEventListener('click', () => {
+        document.querySelector('body').classList.toggle('toggled');
+    })
+})
+document.addEventListener('DOMContentLoaded', (event) => {
+    document.querySelector('.target-burgere').addEventListener('click', () => {
+        document.querySelector('body').classList.toggle('toggled');
+    })
+})
